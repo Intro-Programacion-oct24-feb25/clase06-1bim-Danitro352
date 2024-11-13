b52bb233a38d4e99bd6a3aa3c5894586696563e4
Daniel Idrovo <doidrovo@utpl.edu.ec>
2024-11-13 13:39:16 -0500

    char

--- ejemplos-switch/EjemplosSwitch/src/manejocadenas/EjemploCadenas.java
+++ ejemplos-switch/EjemplosSwitch/src/manejocadenas/EjemploCadenas.java
@@ -13,11 +13,12 @@ public class EjemploCadenas {
     public static void main(String[] args) {
         String ciudad = "loJa";
         ciudad = ciudad.toUpperCase();
-        System.out.printf("%s\n", ciudad.toLowerCase());
+        System.out.printf("%s\n", ciudad.toLowerCase());//no cambia el valor de la variable, solo se imprime lo que pide
         // System.out.printf("%s\n", ciudad.toUpperCase());
-        System.out.printf("%s\n", ciudad);
-        // toLowerCase es un método
-        // toUpperCase es un método
+        System.out.printf("%s\n", ciudad);//se presenta el valor de toUpperCase ya que la varaible ciudad tiene ese valor
+        // toLowerCase es un método, sirve para poner minusculas al valor de la variable
+        // toUpperCase es un método, tambien sirve para poner en mayusculas al valor de la variable
+        
         
     }
 }
